6ce39c102f96648a24db706ea374e006bd70e2ab
Satyansh Singh <satyansh86@gmail.com>
2024-11-27 05:24:54 +0530

    updated

--- frontend/src/pages/EditPost.js
+++ frontend/src/pages/EditPost.js
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch(`http://localhost:4000/post/${id}`)
+    fetch(`https://blog-application-mern-stack.onrender.com/post/${id}`)
       .then(response => response.json())
       .then(postInfo => {
         setTitle(postInfo.title);
@@ -30,7 +30,7 @@ export default function EditPost() {
     if (files) {
       data.set('file', files);
     }
-    const response = await fetch('http://localhost:4000/post', {
+    const response = await fetch('https://blog-application-mern-stack.onrender.com/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
